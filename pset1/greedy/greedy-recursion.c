cc6c8b9d115d75810c091f16e4b88c8eae364705
WG <doubleu.gee10@gmail.com>
2017-09-30 22:32:22 +0200

    refactored some of the code and deleted executable files

--- pset1/greedy/greedy-recursion.c
+++ pset1/greedy/greedy-recursion.c
@@ -2,47 +2,41 @@
 #include <cs50.h>
 #include <math.h>
 
-
-int getUsedCoin (int cents);
-
+int getCoinsNum(int cents);
 
 int main(void)
 {
     float change;
     int coins;
-    
+
     coins = 0;
-    
-    do {
+
+    do
+    {
         change = get_float("yo! how much do you owe me?");
 
-    }while (change < 0);
-       
-       
+    } while (change < 0);
+
     int cents = round(change * 100);
-    
-    coins = getUsedCoin(cents);
-    
-    
+
+    coins = getCoinsNum(cents);
+
     printf("here is your change: %i \n", coins);
 }
 
+int getCoinsNum(int cents)
+{
 
-
-
-int getUsedCoin(int cents) {
-    
     int usedCoin;
     int coinsNum = 0;
-    
-    usedCoin = cents >= 25 ? 25 : cents >= 10 ? 10 : cents >= 5  ? 5  : 1;
-    
-    if (cents > 0) {
+
+    if (cents > 0)
+    {
+
+        usedCoin = cents >= 25 ? 25 : cents >= 10 ? 10 : cents >= 5 ? 5 : 1;
         coinsNum++;
-        return coinsNum + getUsedCoin(cents - usedCoin);
+        return coinsNum + getCoinsNum(cents - usedCoin);
     }
-    
-    
+
     return coinsNum;
-                   
 }
--- pset1/greedy/greedy.c
+++ pset1/greedy/greedy.c
@@ -2,50 +2,48 @@
 #include <cs50.h>
 #include <math.h>
 
-
-int getUsedCoin (int cents);
-
+int getCoinsNum(int cents);
 
 int main(void)
 {
     float change;
-    int coins, usedCoin;
-    
-    // quarter = 25;
-    // dime = 10;
-    // nickle = 5;
-    // penny = 1;
+    int coins, usedCoin, quarter, dime, nickle, penny, coins;
+
+    quarter = 25;
+    dime = 10;
+    nickle = 5;
+    penny = 1;
     coins = 0;
-    
-    
-    do {
+
+    do
+    {
         change = get_float("yo! how much do you owe me?");
 
-    }while (change < 0.01);
-       
-       
+    } while (change < 0);
+
     int cents = round(change * 100);
-    
-    
-    while (cents > 0) {
-        
-        usedCoin = getUsedCoin(cents);
-        
-        cents -=  usedCoin;
+
+    while (cents > 0)
+    {
+
+        usedCoin = getCoinsNum(cents);
+
+        cents -= usedCoin;
         coins++;
     }
-    
-    
+
     printf("here is your change: %i \n", coins);
 }
 
+int getCoinsNum(int cents)
+{
 
-int getUsedCoin(int cents) {
-    
-    if (cents >= 25) return 25;
-    if (cents >= 10) return 10;
-    if (cents >= 5) return 5;
-    
-    return 1;
-                   
+    if (cents >= quarter)
+        return quarter;
+    if (cents >= dime)
+        return dime;
+    if (cents >= nickle)
+        return nickle;
+
+    return penny;
 }
